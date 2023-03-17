7bea976e3efc7e48cf737c4d8f7422924fe839bf
bmarcko <bobbymarckocruz@gmail.com>
2023-03-17 10:35:58 +0800

    first commit

---
+++ PDC03_MODULE3/PDC03_MODULE3/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MODULE3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Activity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity1());
+        }
+        private async void Activity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity2());
+        }
+        private async void Activity03(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity3());
+        }
+        private async void Activity04(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity4());
+        }
+    }
+
+}
